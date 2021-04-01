c0f43288d1d288dc092e123a203450932d9c577a
elis05 <elis05@hotmail.com>
2021-04-01 12:44:09 +0200

    fp

--- front/src/index.js
+++ front/src/index.js
@@ -1,18 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
-
-import 'bootstrap/dist/css/bootstrap.min.css';
-import $ from 'jquery';
-import Popper from 'popper.js';
-import 'bootstrap/dist/js/bootstrap.bundle.min';
-
-
-// import "bootstrap/dist/css/bootstrap.min.css";
-// import "bootstrap/dist/js/bootstrap.min.js";
-// import $ from 'jquery';
-// 	import Popper from 'popper.js';
-// 	import 'bootstrap/dist/js/bootstrap.bundle.min';
+import "bootstrap/dist/css/bootstrap.min.css";
+import $ from "jquery";
+import Popper from "popper.js";
+import "bootstrap/dist/js/bootstrap.bundle.min";
 import "./index.css";
 import App from "./App";
 
